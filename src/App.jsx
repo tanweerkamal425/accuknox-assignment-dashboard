6ca855fc6603ee2ab591eb87d169165f1a363e5b
Tanweer Kamal <tanweerkamal54@gmail.com>
2024-08-14 13:43:50 +0530

    Created header section

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
       <div className="app">
         <Navbar />
       </div>
+      <Header />
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,17 +3,13 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className="header">
-        <div className="header-left">
-            <ul>
-                <li>Home</li>
-                <li>Dashboardv2</li>
-            </ul>
-        </div>
-        <div className="header-right">
-            <div className="search-box">
-                <i className="fa-solid fa-magnifying-glass"></i>
-                <input type="text" className='search' placeholder='Search widget...' />
+    <div className="container">
+        <div className="wrapper">
+            <div className="header">
+                <p className="header-title">CNAPP Dashboard</p> 
+                <div className="header-right">
+                    <button className='widget-button'>+Add Widget</button>
+                </div>           
             </div>
         </div>
     </div>
